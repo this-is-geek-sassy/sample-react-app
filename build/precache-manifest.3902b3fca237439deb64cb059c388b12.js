7812f3edd9dd477b328c2ee6ba3260333ae9d196
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-12-18 15:18:29 +0530

    adding build files

---
+++ build/precache-manifest.3902b3fca237439deb64cb059c388b12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0015eefb7e829632427f6b4e3139b23f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "d5a2140746d4a4e5c89e",
+    "url": "/static/js/3.e13311ee.chunk.js"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/static/js/main.fffdfd66.chunk.js"
+  },
+  {
+    "revision": "704a7695bbc188084118",
+    "url": "/static/js/runtime~main.bd0384ed.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
